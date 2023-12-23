53d7353725b0692875c4f81e15b3f86be87049a9
JackTingleALBD <JackTingleALBD@outlook.com>
2023-12-23 00:26:21 +0000

    projects

---
+++ projects.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.slick-carousel').slick({
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 3,
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        adaptiveHeight: true,
+        mobileFirst: true,
+          });
+    });
